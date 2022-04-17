c4cf63ffbd4545d6b4bbdd3aafb07f5299f556d1
Andres6936 <joan.buritica@artemisa.tech>
2022-04-17 14:42:23 -0500

    Moved Files

---
+++ Source/android/WindowData_Android.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <MiniFB_internal.h>
+
+//-------------------------------------
+typedef struct
+{
+	struct android_app* app;
+	struct mfb_timer* timer;
+} SWindowData_Android;
---
+++ Source/macosx/OSXView.h
@@ -0,0 +1,15 @@
+#import <Cocoa/Cocoa.h>
+
+#include "WindowData.h"
+
+@interface OSXView : NSView <NSTextInputClient>
+{
+@public
+	SWindowData* window_data;
+#if defined(USE_METAL_API)
+	@private NSTrackingArea *tracking_area;
+#endif
+}
+
+@end
+
